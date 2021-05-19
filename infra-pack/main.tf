ba88cbc25106c90956849af74bd2309ad61b154c
fatahnuram <fatahnuram@gmail.com>
2021-05-19 11:35:11 +0700

    infra pack with additional comments

--- infra-pack/main.tf
+++ infra-pack/main.tf
@@ -5,7 +5,7 @@ module "my_vpc" {
 }
 
 module "my_subnet" {
-  source = "github.com/fatahnuram/terraform-module-example.git//vpc?ref=v1.0.0"
+  source = "github.com/fatahnuram/terraform-module-example.git//vpc?ref=v1.1.0"
 
   vpc_id = module.my_vpc.vpc_id
   subnet_cidr = var.subnet_cidr
